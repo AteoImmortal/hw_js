3e5c3738bf2730b7bc6f6af71cfaa7742ccca727
AteoImmortal <105009513+AteoImmortal@users.noreply.github.com>
2023-09-20 20:00:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,106 @@
+/* Задание 1 */
+// let str = 'Привет';
+// let sum = 123;
+// let num = 15.8;
+// let flag = true;
+// let txt = 'true';
+
+// console.log(typeof str)
+// console.log(typeof sum)
+// console.log(typeof num)
+// console.log(typeof flag)
+// console.log(typeof txt)
+
+
+/* Задание 2 */
+// let a1 = 5 % 3; 
+// let a2 = 3 % 5;
+// let a3 = 5 + '3';
+// let a4 = '5' - 3;
+// let a5 = 75 + 'кг';
+// let a6 = 785 * 653;
+let a7 = 100 / 25;
+// let a8 = 0 * 0; 
+// let a9 = 0 / 2;
+// let a10 = 89 / 0;
+// let a11 = 98 + 2;
+// let a12 = 5 - 98;
+// let a13 = (8 + 56 * 4) / 5;
+// let a14 = (9 - 12) * 7 / (5 + 2);
+// let a15 = +"123";
+// let a16 = 1 || 0; 
+// let a17 = false || true;
+// let a18 = true > 0
+
+// console.log(typeof a1);
+// console.log(typeof a2);
+// console.log(typeof a3);
+// console.log(typeof a4);
+// console.log(typeof a5);
+// console.log(typeof a6);
+// console.log(typeof a7);
+// console.log(typeof a8);
+// console.log(typeof a9);
+// console.log(typeof a10);
+// console.log(typeof a11);
+// console.log(typeof a12);
+// console.log(typeof a13);
+// console.log(typeof a14);
+// console.log(typeof a15);
+// console.log(typeof a16);
+// console.log(typeof a17);
+// console.log(typeof a18);
+
+
+/* Задание 3 */
+// let height = 23;
+// let width = 10;
+// let result = height * width;
+// console.log(result);
+
+
+/* Задание 4 */
+// let h = 10;
+// let VCilindra = h * 3.14 * a7 / 4;
+// console.log(VCilindra);
+
+
+/* Задание 5 */
+// let r = 5;
+// let SKruga = 3.14 * 5**2;
+// console.log(SKruga);
+
+
+/* Задание 6 */
+// let a = 5;
+// let b = 7;
+// let h = 10;
+
+// let STrap = (a + b) * h / 2;
+// console.log(STrap)
+
+
+/* Задание 7 */ // Не смог решить 
+// let S = 2;
+// let p = 0.1;
+// let years = 5;
+// let n = years * 5;
+
+// let Pereplata = (S / 12) * ((1 + 1/p*100) / p * 100 * (1 + 1/p*100)) / ((1 + 1/p*100)**n - 1)
+// console.log(Pereplata)
+
+
+/* Задание 8 */
+// let a = 8;
+// let b = 3;
+
+// let equation1 = (16 - a) / 2 + b;
+// console.log('В первом уравнении: a+2(x-b)=16  x =', equation1)
+
+
+// let equation2 = (a - 15 * b) / (b - 6);
+// console.log('Во втором уравнении: b(x+15)=a+6x  x =', equation2)
+
+
+// let equation3 = 23780 / (1 + 2 + a + b)
+// console.log('В третьем уравнении: x+2x+ax+bx=23780  x =', equation3)
