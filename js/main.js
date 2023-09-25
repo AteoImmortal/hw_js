4157af41554570a08fcab54615f91862adc714bc
AteoImmortal <105009513+AteoImmortal@users.noreply.github.com>
2023-09-25 16:13:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,106 +1,185 @@
 /* Задание 1 */
-// let str = 'Привет';
-// let sum = 123;
-// let num = 15.8;
-// let flag = true;
-// let txt = 'true';
+// let nameUser = prompt('Введите ваше имя');
+// let age = prompt('Введите ваш возраст');
+// let city = prompt('Введите ваш город');
+// let phone = prompt('Введите ваш номер телфона');
+// let email = prompt('Введите ваш email');
+// let company = prompt('Введите ваше место работы');
+// let year = 2023 - age;
 
-// console.log(typeof str)
-// console.log(typeof sum)
-// console.log(typeof num)
-// console.log(typeof flag)
-// console.log(typeof txt)
+
+// alert('Меня зовут ' + nameUser +'. Мне  ' + age + 'лет. Я проживаю в горде ' + city + 'и работаю в комании ' + company + '. Мои контактные данные: '+ phone +', ' + email + '.');
+
+
 
 
 /* Задание 2 */
-// let a1 = 5 % 3; 
-// let a2 = 3 % 5;
-// let a3 = 5 + '3';
-// let a4 = '5' - 3;
-// let a5 = 75 + 'кг';
-// let a6 = 785 * 653;
-let a7 = 100 / 25;
-// let a8 = 0 * 0; 
-// let a9 = 0 / 2;
-// let a10 = 89 / 0;
-// let a11 = 98 + 2;
-// let a12 = 5 - 98;
-// let a13 = (8 + 56 * 4) / 5;
-// let a14 = (9 - 12) * 7 / (5 + 2);
-// let a15 = +"123";
-// let a16 = 1 || 0; 
-// let a17 = false || true;
-// let a18 = true > 0
+// alert(nameUser + ' родился в ' + year + ' году.')
 
-// console.log(typeof a1);
-// console.log(typeof a2);
-// console.log(typeof a3);
-// console.log(typeof a4);
-// console.log(typeof a5);
-// console.log(typeof a6);
-// console.log(typeof a7);
-// console.log(typeof a8);
-// console.log(typeof a9);
-// console.log(typeof a10);
-// console.log(typeof a11);
-// console.log(typeof a12);
-// console.log(typeof a13);
-// console.log(typeof a14);
-// console.log(typeof a15);
-// console.log(typeof a16);
-// console.log(typeof a17);
-// console.log(typeof a18);
+
 
 
 /* Задание 3 */
-// let height = 23;
-// let width = 10;
-// let result = height * width;
-// console.log(result);
+// let str = '123456';
+
+// let first = +str[0] + +str[1]; + +str[2];
+// let second = +str[3] + +str[4]; + +str[5];
+
+// if ( first === second){
+//     alert('Да')
+// } else {
+//     alert('Нет')
+// }
+
 
 
 /* Задание 4 */
-// let h = 10;
-// let VCilindra = h * 3.14 * a7 / 4;
-// console.log(VCilindra);
+// let a = +prompt('Введие число');
+// if ( a > 0) {
+//     alert('Верно');
+// } else {
+//     alert('Неверно')
+// }
+
 
 
 /* Задание 5 */
-// let r = 5;
-// let SKruga = 3.14 * 5**2;
-// console.log(SKruga);
+// let a = 10;
+// let b = 2;
+
+// let sum = a + b;
+// let diff = a - b;
+// let mult = a * b;
+// let div = a / b;
+
+// if( sum > 1 ){
+//     sum = sum**2
+// }
+// console.log(sum)
+
 
 
 /* Задание 6 */
-// let a = 5;
-// let b = 7;
-// let h = 10;
+// if (a > 2 && a < 11 || b >= 6 && b < 14) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
 
-// let STrap = (a + b) * h / 2;
-// console.log(STrap)
 
 
-/* Задание 7 */ // Не смог решить 
-// let S = 2;
-// let p = 0.1;
-// let years = 5;
-// let n = years * 5;
+/* Задание 7 */
+// let n = 24;
+// switch(true){
+//     case n >= 0 && n < 15:
+//         console.log('Первая четверть часа');
+//         break;
+//     case n >= 15 && n < 30:
+//         console.log('Вторая четверть часа');
+//         break;
+//     case n >= 30 && n < 45:
+//         console.log('Третья четверть часа');
+//         break;
+//     case n >= 45 && n <= 59:
+//         console.log('Четвёртая четверть часа');
+//         break;
+//     default: 
+//     console.log('Введите корректное число')
+// }
 
-// let Pereplata = (S / 12) * ((1 + 1/p*100) / p * 100 * (1 + 1/p*100)) / ((1 + 1/p*100)**n - 1)
-// console.log(Pereplata)
+
 
 
 /* Задание 8 */
-// let a = 8;
-// let b = 3;
+// let day = 30;
 
-// let equation1 = (16 - a) / 2 + b;
-// console.log('В первом уравнении: a+2(x-b)=16  x =', equation1)
+// switch (true) {
+//     case day > 0 && day <= 10:
+//         console.log('Первая декада месяца');
+//         break;
+//     case day > 10 && day <= 20:
+//         console.log('Вторая декада месяца');
+//         break;
+//     case day > 20 && day <= 30:
+//         console.log('Третья декада месяца');
+//         break;
+//     default:
+//         console.log('Введите корректное число');
+// }
 
 
-// let equation2 = (a - 15 * b) / (b - 6);
-// console.log('Во втором уравнении: b(x+15)=a+6x  x =', equation2)
 
 
-// let equation3 = 23780 / (1 + 2 + a + b)
-// console.log('В третьем уравнении: x+2x+ax+bx=23780  x =', equation3)
+/* Задание 9 */
+// let day = 430;
+// let years;
+// let month;
+// let week;
+// let hour;
+// let minutes;
+// let second;
+
+// if(day >= 365) {
+//     years = day / 365;
+//     console.log(years + ' лет/год');
+// } else {
+//     console.log('меньше года');
+// }
+
+// if(day >= 31) {
+//     month = day / 31;
+//     console.log(month + ' месяцев');
+// } else {
+//     console.log('меньше месяца');
+// }
+
+// if(day >= 7) {
+//     week = day / 7;
+//     console.log(week + ' недель');
+// } else {
+//     console.log('меньше недели');
+// }
+
+// if(day >= 1) {
+//     hour = day * 24;
+//     console.log(hour + ' часов');
+// } else {
+//     console.log('0 часов');
+// }
+
+// if (day >= 1) {
+//     minutes = hour * 60;
+//     console.log(minutes + ' минуты');
+// } else {
+//     console.log('0 минут');
+// }
+
+// if (day >= 1) {
+//     second = minutes * 60;
+//     console.log(second + ' секунды');
+// } else {
+//     console.log('0 cекунд');
+// }
+
+
+
+/* Задание 10 */
+// let day = 249;
+// let month = day / 31;
+
+// switch(true) {
+//     case month >= 0 && month <= 1 || month > 10 && month <= 11:
+//         console.log('зима');
+//         break;
+//     case month > 1 && month <= 4:
+//         console.log('весна');
+//         break;
+//     case month > 4 && month <= 7:
+//         console.log('лето');
+//         break;
+//     case month > 7 && month <= 10:
+//             console.log('осень');
+//             break;
+//     default:
+//         console.log('Введите корректное число');
+// }
