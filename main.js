5a53a43708bc9fe6ea869000358d3376fce12172
AteoImmortal <105009513+AteoImmortal@users.noreply.github.com>
2023-10-07 12:51:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+/* Задача 1 */
+// let a = 10;
+// let b = 4;
+// let c = 2
+// function sum(a, b, c){
+//     return (a - b) / c 
+// }
+// let result = sum(a, b, c)
+// console.log(result)
+
+
+
+/* Задача 2 */
+// let a = 2;
+// function squareCube(a){
+//     let square = a**2;
+//     let cube = a**3;
+//     return {
+//         square: square,
+//         cube: cube
+//     }
+// }
+// let result = squareCube(a);
+// console.log(`Квадрат числа : ${result.square}; Куб числа: ${result.cube}`)
+
+
+
+/* Задача 3 */
+// const a = 13;
+// const b = 9;
+// function minMax(a, b){
+//     if(a > b){
+//         return {
+//             max: a,
+//             min: b
+//         }
+//     } else if(a < b){
+//         return {
+//             max: b,
+//             min: a
+//         }
+//     } else {
+//         return {
+//             max: error,
+//             min: error
+//         }
+//     }
+// }
+// const result = minMax(a, b);
+// console.log(`Минимальное число: ${result.min}`);
+// console.log(`Максимальное число: ${result.max}`)
+
+
+
+/* Задача 4 */
